e8544ad322af7407a084698161ccb9e27f658a60
SRINIVAS DADI <srivardhandadi@gmail.com>
2023-08-05 07:42:51 +0530

    Project Complete

--- src/Pages/Item/Item.jsx
+++ src/Pages/Item/Item.jsx
@@ -1,33 +1,89 @@
 import React,{useEffect,useState} from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import Nav from '../../Components/NavBar/Nav';
+import './Item.css'
+import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai'
 
 const Item = () => {
 
   const {id} = useParams();
-  const [item,setItem] = useState({});
-  const [itemId,setItemId] = useState(null);
+  const [item,setItem] = useState([]);
+  const [fet,setFet] = useState(false);
+  const [count,setCount] = useState(0);
+
 
   useEffect(() => {
     try{
-      setItem(id);
       if(id){
       fetch(`https://fakestoreapi.com/products/${id}`)
       .then((res) => res.json())
-      .then((data) => setItem(data))
-      .then(console.log(item));
+      .then((res) => setItem(res))
+      .then(console.log(item))
+      setFet(true)
       }
     }
     catch(e){
       console.error(e);
     }
-  },[]);
+  },[id]);
+
+  const Dec = () => {
+    if(count === 0)
+      return
+    else{
+      setCount(count-1);
+    }
+  }
+
+  const Inc =() => {
+    if(count === 5){
+      return
+    }
+    setCount(count+1);
+  }
+
+
 
   return (
     <div>
-      <h2>Item Details</h2>
-      {/* <p>Title: {item.title}</p>
-      <p>Description: {item.description}</p> */}
-      {/* Add more properties as needed */}
+      <Nav/>
+      {fet &&
+      <div>
+      <hr className='line'/>
+      <div className="ItemPath">
+        <p> <Link to='/' >Product/Item/ </Link> </p> <p> {item.title}</p>
+      </div>
+      <div className="Item">
+        <div className="ItemImg">
+          <img src={item.image} alt="" />
+        </div>
+        <div className="ItemDetails">
+          <p className='ItemTitle' >{item.title}</p>
+          <p className='ItemDesc'>{item.description}</p>
+          <hr className='line'/>
+          <div className="ItemPrice">
+          {item.price > 50 ? <p>$ {item.price} or $10/month <br /> <span>suggested payments with credit cards</span> </p> : <p> ${item.price} </p> }
+          </div>
+          <hr className='line'/>
+          <div className="OrderCount">
+            <div>
+              <h2 onClick={Dec} > <AiOutlineMinus /> </h2>
+              <p>{count}</p>
+              <h2 onClick={Inc}> <AiOutlinePlus /> </h2>
+            </div>
+            <p>Select Quantity</p>
+          </div>
+          <hr className='line'/>
+          <div className="Buttons">
+            <p className='Buy'>Buy Now</p>
+            <p className='AddToCart'>Add To Cart</p>
+          </div>
+          div.
+        </div>
+      </div>
+      </div>
+      }
+      {!fet && <p>loading</p> }
     </div>
   )
 }
